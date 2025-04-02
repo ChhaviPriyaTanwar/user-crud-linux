c2d3fc1d5fcfa2ceab03e4da7a0c282375d35503
Chhavi Priya Tanwar <160010758+ChhaviPriyaTanwar@users.noreply.github.com>
2025-04-02 11:26:18 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { Dialect, Number } from 'sequelize';
+import { Dialect } from 'sequelize';
 import { UsersModule } from './users/users.module';
 
 @Module({
